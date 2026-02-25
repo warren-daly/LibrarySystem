ac6cd82be76f4e906fe09a2df06c38cd8d49a46c
X00212600 Warren Daly <X00212600@mytudublin.ie>
2026-02-25 15:12:40 +0000

    Added nav login button dropdown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
+
+<style>
+.dropdown {
+	position: absolute;
+	right: 1em;
+	display: inline-block;
+}
+
+.dropdown-content {
+	background-color: white;
+	display: none;
+	right: 15%;
+	position: absolute;
+}
+
+.dropdown:hover .dropdown-content {display: block;}
+</style>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
@@ -14,8 +30,19 @@
 	<a href="/Catalogue">Catalogue</a>
     <a href="/About">About</a>
     <a href="/Contact">Contact</a>
-    <a href="/Login">Login</a>
-	
+
+	<div class="dropdown">
+		<button class="dropdown-btn">Login</button>
+		<div class="dropdown-content">
+			<form>
+				<label for="username">User Name:</label>
+				<input type="text" id="username" name="username">
+				<label for="password">Password:</label>
+				<input type="password" id="password" name="password">
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</div>
 </nav>
 
 {@render children()}
