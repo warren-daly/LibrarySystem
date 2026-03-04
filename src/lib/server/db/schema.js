7535b4811dd714a3d84279e92a88396dc471f2cc
Lauren Metcalfe <x00228510@mytudublin.ie>
2026-03-04 23:41:19 +0000

    TEMP DATABASE

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,9 +1,15 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-export const task = sqliteTable('task', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+export const book = sqliteTable('book', {
+  id: integer().primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  author: text().notNull(),
+  description: text(),
+  price: integer().notNull(), // stored in cents
+  image: text(),
+  stock: integer().notNull().default(0),
+  createdAt: integer({ mode: 'timestamp_ms' })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`)
 });
